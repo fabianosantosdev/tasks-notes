12d08a5fb6b1228a5c2cfdbe6ccfffc42857fc0f
fabianosantosdev <69941827+fabianosantosdev@users.noreply.github.com>
2020-08-24 20:30:27 -0300

    Update Javascript code
    Add a new feature that enables get the data of when the task was made and fix a bug at the message screen

--- app.js
+++ app.js
@@ -1,99 +1,119 @@
-//Made by Fabiano Barbosa dos Santos, Year: 2020
-// Web app that allow you make one list of your tasks, and close when they was complete.
+/*
+  Made by Fabiano Santos. 
+  Version: 0.2
+  Year 2020
+*/
 
-// Object responsible for the 'Task' and the color
-let task = {
-	HTMLdom :"<div class='task' id='%color%'><span class='task--close'>x</span><p class='task__description' contenteditable='true'> %textContent% </p></div>",
-	taskList : [],
-	colorSelected : "",
-	makeNewTask : function (color, text) {
-		this.color = color;
-		this.text = text;
-		let newHTMLdom = this.HTMLdom;
-		newHTMLdom = newHTMLdom.replace("%color%", color);
-		newHTMLdom = newHTMLdom.replace("%textContent%", text);
-		ui.showTaskContent(newHTMLdom);
-		ui.cleanForm();
-		ui.taskClose();
-		this.taskList.push(newHTMLdom);
-},
-	getColor : function () {
-		let colors = document.querySelectorAll(".color");
-		for (var i = 0; i < colors.length; i++) {
-			colors[i].addEventListener("click", function () {
-				task.colorSelected = this.id;
-			})
-		}
-	}
-};
+//Main anonymous function responsible by the data and the main method
+var app = (function () {
+  data = {
+    htmlDom : "<div class='task' id='%color%'><span class='task__date'> %date% </span><span class='task--close'>x</span><p class='task__description' contenteditable='true'> %textContent% </p></div>",
+    taskList : [],
+    colorSelected : "",
+    taskListDom  : document.querySelector(".task-list"),
+    newTaskInput : document.querySelector(".new-task__description").value
+  };
 
-// Object that control the user interface, 
-let ui = {
-	taskListDom : document.querySelector(".task-list"),
-	showTaskContent : function (content) {
-		this.taskListDom.insertAdjacentHTML("beforeend", content)
-	},	
+  dayAndHour = function () {
+    now = new Date().toString().slice(0,21);
+    return (now)
+  };
 
-	//Allow close an concluied task
-	taskClose : function () {
-		let closeIcon = document.querySelectorAll(".task--close");
-		for (var i = 0; i < closeIcon.length; i++) {
-			closeIcon[i].addEventListener("click", function (task) {
-				task.target.parentNode.remove();
-			} )
-		} 
-	},
+  return {
+    data :  {
+      colorSelected : data.colorSelected,
+      taskListDom : data.taskListDom,
+      taskList : []
+  },
+  
+    makeNewTask : function (color, text, date) {
+      this.color = color;
+      this.text = text;
+      this.date = date;
+      var newHTMLDom = data.htmlDom;
+      newHTMLDom = newHTMLDom.replace("%color%", color);
+      newHTMLDom = newHTMLDom.replace("%textContent%", text);
+      newHTMLDom = newHTMLDom.replace("%date%", dayAndHour());
+      return (newHTMLDom);
+    },
 
-	//After one new task was made, the method clean the form input
-	cleanForm : function () {
-		document.querySelector(".new-task__description").value = "";
-		task.colorSelected = "";
-		this.taskClose();
-	},
-
-	//Method that show some message at the screen 
-	showErrorMsg : function (text) {
-		let getDiv = document.querySelector(".error-msg");
-		getDiv.id = "";
-		let getDivContent = document.querySelector(".error-msg__content");
-		closeDiv = document.querySelector(".error-msg--close").addEventListener("click", function (close) {
-			close.target.parentNode.remove();
-		});
-		getDivContent.innerHTML = text;
-		setTimeout( function () {
-			getDiv.id = "error-msg__hide";
-		}, 8000);
-	},
-
-	//Check if the button was clicked or the 'Enter' was pressed
-	checkInput : function () {
-		if (document.querySelector(".new-task__description").value == "") {
-			ui.showErrorMsg("You must writter something");
-		}else {
-			check = function(checkOf) {
-			console.log(checkOf);
-			if (checkOf.innerHText in checkOf == document.querySelector(".new-task__description").value) {
-				console.log("task already exist");
-			}
-			}
-			check(document.querySelector(".new-task__description"));	
-			newTask = task.makeNewTask(task.colorSelected, document.querySelector(".new-task__description").value);
-			}
-	}
-}
-
-//Anonymous function that start the app, with the necessary feature and catch the input elements
-appStart = ( function () {
-	ui.taskClose();
-	task.getColor();
-		document.querySelector(".new-task__description").addEventListener("keypress", function (e) {
-		if (e.keyCode == 13) {
-			ui.checkInput();
-		}
-		
-	});
-	document.querySelector(".new-task__submit").addEventListener("click", function () {
-		ui.checkInput();
-	});	
-
+    getTaskColor : function () {
+      let colors = document.querySelectorAll('.color');
+      for (var i = 0; i < colors.length; i++) {
+        colors[i].addEventListener("click", function () {
+        app.data.colorSelected = this.id;
+       });
+      }
+    }
+  }
 })();
+
+
+//Anonymous function responsible by the user interface(delete and add new task or even a message on the screen)
+var uiControl = (function (app){
+  showTaskDom = function (content) {
+    app.data.taskListDom.insertAdjacentHTML("beforeend", content);
+  };
+
+  closeTask = function () {
+    let closeIcon = document.querySelectorAll(".task--close");
+    for (var i = 0; i < closeIcon.length; i++) {
+      closeIcon[i].addEventListener("click", function (task) {
+        task.target.parentNode.remove();
+      });
+    }
+  };
+
+  cleanForm = function () {
+    document.querySelector(".new-task__description").value = "";
+    app.data.colorSelected = "";
+
+  };
+
+  checkInputData = function () {
+    if (document.querySelector(".new-task__description").value === "") {
+      console.log(document.querySelector(".new-task__description").value);
+      showMsg("You must writter something");
+    }else {
+        newTask = app.makeNewTask(app.data.colorSelected, document.querySelector(".new-task__description").value);
+        showTaskDom(newTask);
+        cleanForm();
+        closeTask();
+      }
+           
+  };
+
+  showMsg = function (textContent) {
+    var getDomElement = document.querySelector(".error-msg");
+    var getDomElementContent = document.querySelector(".error-msg__content");
+    var hideDomElement = document.querySelector(".error-msg--close");
+    getDomElement.id = "";
+    getDomElementContent.innerHTML = textContent;
+
+    hideDomElement.addEventListener("click", function (element) {
+      element.target.parentNode.id = "error-msg__hide";
+    });
+    setTimeout( function () {
+      getDomElement.id = "error-msg__hide";
+    }, 8000);
+
+  };
+  return {
+    appStart : function () {
+      closeTask();
+      app.getTaskColor();
+      document.querySelector(".new-task__description").addEventListener("keypress", function (e) {
+        if (e.keyCode == 13) {
+            checkInputData();
+        }
+      });
+      document.querySelector(".new-task__submit").addEventListener("click", function () {
+        checkInputData();
+      });
+
+    }
+  }
+})(app);
+
+
+uiControl.appStart();
